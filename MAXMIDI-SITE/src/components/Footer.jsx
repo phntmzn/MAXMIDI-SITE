2203044c6f53ed86b5405cd4e21748e2fc5c5dbb
zoned out <126989297+phntmzn@users.noreply.github.com>
2025-07-29 19:31:12 -0500

    Delete MAXMIDI-SITE directory

--- MAXMIDI-SITE/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-plum text-lavender text-center py-6 mt-12">
-      <p>&copy; {new Date().getFullYear()} MaxMidi Programs. All rights reserved.</p>
-    </footer>
-  );
-}
